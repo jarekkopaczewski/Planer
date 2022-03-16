b461e79947813e7cf452044bf666f41299cb7093
Mikolaj Szymczyk <mikolaj.szymczyk.99@gmail.com>
2022-03-16 13:18:55 +0100

    add switch to view and some listener to him, also add data picker dialog

--- app/src/main/java/skills/future/planer/ui/tasklist/TaskListCreatorFragment.java
+++ app/src/main/java/skills/future/planer/ui/tasklist/TaskListCreatorFragment.java
@@ -1,5 +1,6 @@
 package skills.future.planer.ui.tasklist;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,7 +10,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Switch;
+
+import java.util.Calendar;
 
 import skills.future.planer.R;
 
@@ -29,6 +33,9 @@ public class TaskListCreatorFragment extends Fragment {
     private String mParam1;
     private String mParam2;
     private Button saveButton;
+    private final Calendar myCalendar = Calendar.getInstance();
+    private EditText editText;
+    private Switch switchDate;
 
     public TaskListCreatorFragment() {
         // Required empty public constructor
@@ -67,13 +74,54 @@ public class TaskListCreatorFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_task_list_creator, container, false);
 
-
+        switchDate = view.findViewById(R.id.SwitchDatePicker);
+        switchDate.setChecked(false);
+        switchDate.setOnCheckedChangeListener((compoundButton, b) -> {
+            editText.setVisibility(b ? View.VISIBLE : View.INVISIBLE);
+        });
         saveButton = view.findViewById(R.id.saveCreatorButton);
+        saveBtnOnClickListenerSetter();
+        editText = (EditText) view.findViewById(R.id.editTextDate);
+        editTextSetter();
+
+        return view;
+    }
+
+    private void saveBtnOnClickListenerSetter() {
         saveButton.setOnClickListener(view1 -> {
             Navigation.findNavController(view1)
                     .navigate(TaskListCreatorFragmentDirections.actionTaskListCreatorFragmentToNavTaskList());
         });
+    }
 
-        return view;
+    private void editTextSetter() {
+        DatePickerDialog.OnDateSetListener date = (datePicker, i, i1, i2) -> {
+            myCalendar.set(Calendar.YEAR, i);
+            myCalendar.set(Calendar.MONTH, i1);
+            myCalendar.set(Calendar.DAY_OF_MONTH, i2);
+            updateLabel();
+        };
+        editText.setOnClickListener(view12 -> new DatePickerDialog(this.getContext(), date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show());
+    }
+
+    private void updateLabel() {
+//        Locale locale = new Locale("en", "UK");
+//        DateFormatSymbols dateFormatSymbols = new DateFormatSymbols(locale);
+//        dateFormatSymbols.setWeekdays(new String[]{
+//                "Unused",
+//                "Niedz",
+//                "Pon",
+//                "Wt",
+//                "ŚR",
+//                "Czw",
+//                "Pt",
+//                "Sob",
+//        });
+//
+//        String pattern = "EEEEE MMMMM yyyy";
+//        SimpleDateFormat simpleDateFormat =
+//                new SimpleDateFormat(pattern, dateFormatSymbols);
+        editText.setText(myCalendar.getTime().toString());
+        //todo ustawić lepszy format daty
     }
 }
