320cdaf1db85bb65f6dbc8045feded526575d08d
Mikolaj Szymczyk <mikolaj.szymczyk.99@gmail.com>
2022-04-02 01:44:37 +0200

    add TaskDataRepository and TaskDataViewModel add Populate async task

---
+++ app/src/main/java/skills/future/planer/db/task/TaskDataRepository.java
@@ -0,0 +1,44 @@
+package skills.future.planer.db.task;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+import skills.future.planer.db.AppDatabase;
+
+
+public class TaskDataRepository {
+    private TaskDataDao taskDataDao;
+    private LiveData<List<TaskData>> listLiveData;
+
+    TaskDataRepository(Application application){
+        AppDatabase db = AppDatabase.getInstance(application);
+        taskDataDao = db.taskDataTabDao();
+        listLiveData = taskDataDao.getTaskData();
+    }
+
+    void insert(TaskData taskData){
+        new InsertAsyncTask(taskDataDao).execute(taskData);
+    }
+
+    LiveData<List<TaskData>> getAllTaskData() {
+        return listLiveData;
+    }
+
+    private static class InsertAsyncTask extends AsyncTask<TaskData,Void,Void> {
+        private final TaskDataDao asyncTaskDao;
+        InsertAsyncTask(TaskDataDao taskDataDao){
+            asyncTaskDao = taskDataDao;
+        }
+
+        @Override
+        protected Void doInBackground(final TaskData... params){
+            asyncTaskDao.insert(params[0]);
+            return null;
+        }
+    }
+
+}
