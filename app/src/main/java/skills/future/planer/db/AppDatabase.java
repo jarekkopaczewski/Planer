ea9eca5049cf9b0e7329770f77cbb76e04f830c6
Mikolaj Szymczyk <mikolaj.szymczyk.99@gmail.com>
2022-04-03 01:01:08 +0200

    remove data to testing

--- app/src/main/java/skills/future/planer/db/AppDatabase.java
+++ app/src/main/java/skills/future/planer/db/AppDatabase.java
@@ -32,7 +32,7 @@ public abstract class AppDatabase extends RoomDatabase {
                     Log.d(LOG_TAG, "Creating new database instance");
                     sInstance = Room.databaseBuilder(context.getApplicationContext(),
                             AppDatabase.class, DB_NAME).fallbackToDestructiveMigration().allowMainThreadQueries()
-                            .addCallback(sRoomDatabaseCallback).build();
+                            /*.addCallback(sRoomDatabaseCallback)*/.build();
                 }
             }
         }
