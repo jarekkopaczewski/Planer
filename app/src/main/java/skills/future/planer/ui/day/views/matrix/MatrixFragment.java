f20a0f9cf2d723ed90d3f8081f62068c0b742b02
Jarek <jarekopaczewski@protonmail.com>
2022-04-10 20:57:05 +0200

    - add matrix view

--- app/src/main/java/skills/future/planer/ui/day/views/matrix/MatrixFragment.java
+++ app/src/main/java/skills/future/planer/ui/day/views/matrix/MatrixFragment.java
@@ -1,113 +1,126 @@
 package skills.future.planer.ui.day.views.matrix;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.Navigation;
-import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import java.util.ArrayList;
 
-import skills.future.planer.R;
 import skills.future.planer.databinding.FragmentMatrixBinding;
 import skills.future.planer.db.task.TaskData;
 import skills.future.planer.db.task.TaskDataViewModel;
-import skills.future.planer.db.task.enums.priority.Priorities;
-import skills.future.planer.db.task.enums.priority.TimePriority;
-import skills.future.planer.ui.AnimateView;
-import skills.future.planer.ui.tasklist.TaskListFragmentDirections;
 
 public class MatrixFragment extends Fragment {
 
     private FragmentMatrixBinding binding;
     private MatrixModelView matrixModelView;
-    private RecyclerView listTotal;
-    private RecyclerView listTotal2;
-    private RecyclerView listTotal3;
-    private RecyclerView listTotal4;
-    private MatrixTotalAdapter taskTotalAdapter;
-    private MatrixTotalAdapter taskTotalAdapter2;
-    private MatrixTotalAdapter taskTotalAdapter3;
-    private MatrixTotalAdapter taskTotalAdapter4;
-    private TaskDataViewModel mWordViewModel;
-    private TaskDataViewModel mWordViewModel2;
-    private TaskDataViewModel mWordViewModel3;
-    private TaskDataViewModel mWordViewModel4;
+    private ArrayList<RecyclerView> recyclerViews;
+    private ArrayList<MatrixListTotalAdapter> matrixAdapters;
+    private ArrayList<TaskDataViewModel> taskDataViewModels;
+    private ArrayList<ProgressBar> progressBars;
 
     public MatrixFragment() {
     }
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         binding = FragmentMatrixBinding.inflate(inflater, container, false);
+        matrixModelView = new ViewModelProvider(this).get(MatrixModelView.class);
         View root = binding.getRoot();
 
-        taskTotalAdapter = new MatrixTotalAdapter(this.getContext(), Priorities.Important, TimePriority.Urgent);
-        taskTotalAdapter2 = new MatrixTotalAdapter(this.getContext(), Priorities.Important, TimePriority.NotUrgent);
-        taskTotalAdapter3 = new MatrixTotalAdapter(this.getContext(), Priorities.NotImportant, TimePriority.Urgent);
-        taskTotalAdapter4 = new MatrixTotalAdapter(this.getContext(), Priorities.NotImportant, TimePriority.NotUrgent);
+        // create array lists
+        matrixAdapters = new ArrayList<>();
+        taskDataViewModels = new ArrayList<>();
+        recyclerViews = new ArrayList<>();
+        progressBars = new ArrayList<>();
 
-        listTotal = binding.testList;
-        listTotal2 = binding.testList2;
-        listTotal3 = binding.testList3;
-        listTotal4 = binding.testList4;
+        // progress bars binding
+        progressBars.add(binding.urgentImportantProgressBar);
+        progressBars.add(binding.urgentNotImportantProgressBar);
+        progressBars.add(binding.notUrgentImportantProgressBar);
+        progressBars.add(binding.notUrgentNotImportantProgressBar);
 
-        listTotal.setAdapter(taskTotalAdapter);
-        listTotal.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        // recycler binding
+        recyclerViews.add(binding.urgentImportantRecycler);
+        recyclerViews.add(binding.urgentNotImportantRecycler);
+        recyclerViews.add(binding.notUrgentImportantRecycler);
+        recyclerViews.add(binding.notUrgentNotImportantRecycler);
 
-        listTotal2.setAdapter(taskTotalAdapter);
-        listTotal2.setLayoutManager(new LinearLayoutManager(this.getContext()));
-
-        listTotal3.setAdapter(taskTotalAdapter);
-        listTotal3.setLayoutManager(new LinearLayoutManager(this.getContext()));
-
-        listTotal4.setAdapter(taskTotalAdapter);
-        listTotal4.setLayoutManager(new LinearLayoutManager(this.getContext()));
-
-        mWordViewModel = new ViewModelProvider(this).get(TaskDataViewModel.class);
-        mWordViewModel2 = new ViewModelProvider(this).get(TaskDataViewModel.class);
-        mWordViewModel3 = new ViewModelProvider(this).get(TaskDataViewModel.class);
-        mWordViewModel4 = new ViewModelProvider(this).get(TaskDataViewModel.class);
-
-        mWordViewModel.getAllTaskData().observe(this.getViewLifecycleOwner(), taskData -> taskTotalAdapter.setFilteredTaskList(taskData));
-        mWordViewModel2.getAllTaskData().observe(this.getViewLifecycleOwner(), taskData -> taskTotalAdapter2.setFilteredTaskList(taskData));
-        mWordViewModel3.getAllTaskData().observe(this.getViewLifecycleOwner(), taskData -> taskTotalAdapter2.setFilteredTaskList(taskData));
-        mWordViewModel4.getAllTaskData().observe(this.getViewLifecycleOwner(), taskData -> taskTotalAdapter2.setFilteredTaskList(taskData));
-
-
-        getParentFragmentManager().setFragmentResultListener("requestKey", this, (requestKey, bundle) -> {
-            TaskData result = bundle.getParcelable("bundleKey");
-            mWordViewModel.insert(result);
-        });
-
-        getParentFragmentManager().setFragmentResultListener("requestKey", this, (requestKey, bundle) -> {
-            TaskData result = bundle.getParcelable("bundleKey");
-            mWordViewModel2.insert(result);
-        });
-
-        getParentFragmentManager().setFragmentResultListener("requestKey", this, (requestKey, bundle) -> {
-            TaskData result = bundle.getParcelable("bundleKey");
-            mWordViewModel3.insert(result);
-        });
-
-        getParentFragmentManager().setFragmentResultListener("requestKey", this, (requestKey, bundle) -> {
-            TaskData result = bundle.getParcelable("bundleKey");
-            mWordViewModel4.insert(result);
-        });
-
-        taskTotalAdapter.getFilter().filter(" ");
-        taskTotalAdapter2.getFilter().filter(" ");
-        taskTotalAdapter3.getFilter().filter(" ");
-        taskTotalAdapter4.getFilter().filter(" ");
+        setUpAdapters();
+        setUpModels();
 
         return root;
+    }
+
+    /**
+     * Creates and sets up adapters in loop
+     * Add view
+     */
+    public void setUpAdapters() {
+        for (int i = 0; i < 4; i++) {
+            matrixAdapters.add(new MatrixListTotalAdapter(this.getContext()));
+            recyclerViews.get(i).setAdapter(matrixAdapters.get(i));
+            recyclerViews.get(i).setLayoutManager(new LinearLayoutManager(this.getContext()));
+            taskDataViewModels.add(new ViewModelProvider(this).get(TaskDataViewModel.class));
+        }
+    }
+
+    /**
+     * Adds live data observers & fragmentResultsListeners
+     */
+    public void setUpModels() {
+        // nie jest w pętli bo lambdy i trzeba zmienić getter dla każdego modelu
+        taskDataViewModels.get(0).getAllTaskData().observe(this.getViewLifecycleOwner(), taskData -> {
+            matrixAdapters.get(0).setFilteredTaskList(taskData);
+            progressBars.get(0).setMax(matrixAdapters.get(0).getItemCount());
+            progressBars.get(0).setProgress(matrixAdapters.get(0).getDone());
+        });
+
+        taskDataViewModels.get(1).getAllTaskData().observe(this.getViewLifecycleOwner(), taskData -> {
+            matrixAdapters.get(1).setFilteredTaskList(taskData);
+            progressBars.get(1).setMax(matrixAdapters.get(1).getItemCount());
+            progressBars.get(1).setProgress(matrixAdapters.get(1).getDone());
+        });
+
+        taskDataViewModels.get(2).getAllTaskData().observe(this.getViewLifecycleOwner(), taskData -> {
+            matrixAdapters.get(2).setFilteredTaskList(taskData);
+            progressBars.get(2).setMax(matrixAdapters.get(2).getItemCount());
+            progressBars.get(2).setProgress(matrixAdapters.get(2).getDone());
+        });
+
+        taskDataViewModels.get(3).getAllTaskData().observe(this.getViewLifecycleOwner(), taskData -> {
+            matrixAdapters.get(3).setFilteredTaskList(taskData);
+            progressBars.get(3).setMax(matrixAdapters.get(3).getItemCount());
+            progressBars.get(3).setProgress(matrixAdapters.get(3).getDone());
+        });
+
+        getParentFragmentManager().setFragmentResultListener("requestKey", this, (requestKey, bundle) -> {
+            TaskData result = bundle.getParcelable("bundleKey");
+            taskDataViewModels.get(0).insert(result);
+        });
+
+        getParentFragmentManager().setFragmentResultListener("requestKey", this, (requestKey, bundle) -> {
+            TaskData result = bundle.getParcelable("bundleKey");
+            taskDataViewModels.get(1).insert(result);
+        });
+
+        getParentFragmentManager().setFragmentResultListener("requestKey", this, (requestKey, bundle) -> {
+            TaskData result = bundle.getParcelable("bundleKey");
+            taskDataViewModels.get(2).insert(result);
+        });
+
+        getParentFragmentManager().setFragmentResultListener("requestKey", this, (requestKey, bundle) -> {
+            TaskData result = bundle.getParcelable("bundleKey");
+            taskDataViewModels.get(3).insert(result);
+        });
     }
 
     @Override
--- app/src/main/java/skills/future/planer/ui/tasklist/Colors.java
+++ app/src/main/java/skills/future/planer/ui/tasklist/Colors.java
@@ -4,40 +4,20 @@ import android.graphics.Color;
 
 public enum Colors {
     RED(81, 205, 197),
-    BLUE(198, 162, 202),
-    YELLOW(0, 126, 143),
+    BLUE(0, 126, 143),
+    YELLOW(198, 162, 202),
     PINK(113, 83, 151);
 
     private final int r;
     private final int g;
     private final int b;
-    private final String rgb;
 
-    private Colors(final int r,final int g,final int b) {
+    Colors(final int r, final int g, final int b) {
         this.r = r;
         this.g = g;
         this.b = b;
-        this.rgb = r + ", " + g + ", " + b;
     }
 
-    public String getRGB() {
-        return rgb;
-    }
-
-    //You can add methods like this too
-    public int getRed(){
-        return r;
-    }
-
-    public int getGreen(){
-        return g;
-    }
-
-    public int getBlue(){
-        return r;
-    }
-
-    //Or even these
     public int getColor(){
         return Color.rgb(r,g,b);
     }
