b52f744a17c45d040a3ea6b7d49c4359e363e570
Mikolaj Szymczyk <mikolaj.szymczyk.99@gmail.com>
2022-05-05 20:59:39 +0200

    add saving habits

--- app/src/main/java/skills/future/planer/ui/habit/HabitCreatorActivity.java
+++ app/src/main/java/skills/future/planer/ui/habit/HabitCreatorActivity.java
@@ -8,9 +8,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.chip.Chip;
 import com.google.android.material.chip.ChipGroup;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.skydoves.powerspinner.PowerSpinnerView;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -18,6 +21,11 @@ import java.util.Locale;
 
 import skills.future.planer.R;
 import skills.future.planer.databinding.ActivityHabitCreatorBinding;
+import skills.future.planer.db.DataBaseException;
+import skills.future.planer.db.habit.HabitData;
+import skills.future.planer.db.habit.HabitDuration;
+import skills.future.planer.db.habit.HabitViewModel;
+import skills.future.planer.tools.DatesParser;
 
 
 public class HabitCreatorActivity extends AppCompatActivity {
@@ -32,6 +40,9 @@ public class HabitCreatorActivity extends AppCompatActivity {
     private ChipGroup daysChipGroupTwo;
     private FloatingActionButton saveCreatorButtonHabit;
     private EditText editTextTitle;
+    private HabitViewModel habitViewModel;
+    private PowerSpinnerView habitDurationSpinner;
+    private Chip MondayChip, TuesdayChip, WednesdayChip, ThursdayChip, FridayChip, SaturdayChip, SundayChip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +64,9 @@ public class HabitCreatorActivity extends AppCompatActivity {
         editTextDateHabit.setText(formatterDate.format(calendar2.getTime()));
 
         // sets first item selected in check boxes
-        binding.spinner.selectItemByIndex(0);
+        habitDurationSpinner = binding.spinner;
+        habitDurationSpinner.selectItemByIndex(0);
+
         binding.spinner2.selectItemByIndex(0);
 
         // add on click & time change listener
@@ -64,6 +77,17 @@ public class HabitCreatorActivity extends AppCompatActivity {
         saveHabitButtonSetUp();
 
         setTitle(R.string.habitsTitle);
+
+        MondayChip = binding.MondayChip;
+        TuesdayChip = binding.TuesdayChip;
+        WednesdayChip = binding.WednesdayChip;
+        ThursdayChip = binding.ThursdayChip;
+        FridayChip = binding.FridayChip;
+        SaturdayChip = binding.SaturdayChip;
+        SundayChip = binding.SunDayChip;
+
+
+        habitViewModel = new ViewModelProvider(this).get(HabitViewModel.class);
     }
 
     /**
@@ -76,6 +100,26 @@ public class HabitCreatorActivity extends AppCompatActivity {
             } else if (daysChipGroupOne.getCheckedChipIds().size() == 0 && daysChipGroupTwo.getCheckedChipIds().size() == 0) {
                 Toast.makeText(this, R.string.habit_error_2, Toast.LENGTH_SHORT).show();
             } else {
+                try {
+                    var tab = getResources().getStringArray(R.array.temp_array);
+                    HabitDuration duration = switch (Integer.parseInt(tab[habitDurationSpinner.getSelectedIndex()].split(" ")[0])) {
+                        case 21 -> HabitDuration.UltraShort;
+                        case 90 -> HabitDuration.Short;
+                        case 120 -> HabitDuration.Long;
+                        default -> throw new IllegalStateException("Unexpected value: " + Integer.parseInt(tab[habitDurationSpinner.getSelectedIndex()].split(" ")[0]));
+                    };
+                    String weekDays = (MondayChip.isChecked() ? "1" : "0") +
+                            (TuesdayChip.isChecked() ? "1" : "0") +
+                            (WednesdayChip.isChecked() ? "1" : "0") +
+                            (ThursdayChip.isChecked() ? "1" : "0") +
+                            (FridayChip.isChecked() ? "1" : "0") +
+                            (SaturdayChip.isChecked() ? "1" : "0") +
+                            (SundayChip.isChecked() ? "1" : "0");
+                    var habit = new HabitData(editTextTitle.getText().toString(), weekDays, duration, DatesParser.toLocalDate(calendar2.getTime()));
+                    habitViewModel.insert(habit);
+                } catch (DataBaseException dataBaseException) {
+                    dataBaseException.printStackTrace();
+                }
                 finish();
             }
         });
@@ -91,7 +135,9 @@ public class HabitCreatorActivity extends AppCompatActivity {
             timeEditText.setText(formatter.format(calendar.getTime()));
         };
 
-        timeEditText.setOnClickListener(e -> new TimePickerDialog(this, time, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true).show());
+        timeEditText.setOnClickListener(e -> new TimePickerDialog(this,
+                time, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE),
+                true).show());
     }
 
     /**
@@ -103,6 +149,8 @@ public class HabitCreatorActivity extends AppCompatActivity {
             editTextDateHabit.setText(formatterDate.format(calendar2.getTime()));
         };
 
-        editTextDateHabit.setOnClickListener(e -> new DatePickerDialog(this, date, calendar2.get(Calendar.YEAR), calendar2.get(Calendar.MONTH), calendar2.get(Calendar.DAY_OF_MONTH)).show());
+        editTextDateHabit.setOnClickListener(e -> new DatePickerDialog(this,
+                date, calendar2.get(Calendar.YEAR), calendar2.get(Calendar.MONTH),
+                calendar2.get(Calendar.DAY_OF_MONTH)).show());
     }
 }
