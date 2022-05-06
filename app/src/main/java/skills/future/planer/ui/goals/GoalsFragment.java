8a0f38424b9c58ba0c1c0827907154a7dad8505a
Jarek <jarekopaczewski@protonmail.com>
2022-05-06 21:40:20 +0200

    - add position label to view pager & animation

--- app/src/main/java/skills/future/planer/ui/goals/GoalsFragment.java
+++ app/src/main/java/skills/future/planer/ui/goals/GoalsFragment.java
@@ -1,9 +1,11 @@
 package skills.future.planer.ui.goals;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -12,6 +14,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.codeboy.pager2_transformers.Pager2_BackDrawTransformer;
 import com.codeboy.pager2_transformers.Pager2_CubeInTransformer;
 import com.codeboy.pager2_transformers.Pager2_CubeOutTransformer;
 import com.codeboy.pager2_transformers.Pager2_DepthTransformer;
@@ -26,6 +29,7 @@ public class GoalsFragment extends Fragment {
     private FragmentGoalsBinding binding;
     private GoalTotalAdapter goalTotalAdapter;
     private ViewPager2 totalGoalList;
+    private TextView pagerCountText;
 
     public GoalsFragment() {
     }
@@ -37,15 +41,33 @@ public class GoalsFragment extends Fragment {
         View root = binding.getRoot();
 
         totalGoalList = binding.totalGoalList;
+        pagerCountText = binding.pagerCountText;
+
         goalTotalAdapter = new GoalTotalAdapter(this.getContext());
         goalTotalAdapter.setLifecycle(getLifecycle());
         goalTotalAdapter.setFragmentManager(getChildFragmentManager());
 
+
         totalGoalList.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
         totalGoalList.setAdapter(goalTotalAdapter);
+        totalGoalList.setPageTransformer(new CustomTransformer());
+        totalGoalList.setClipToPadding(false);
+        totalGoalList.setClipChildren(false);
+        totalGoalList.setClipToPadding(false);
+        totalGoalList.setOverScrollMode(2);
+        totalGoalList.setPadding(50, 0, 50, 0);
         totalGoalList.setOffscreenPageLimit(3);
-        totalGoalList.setPageTransformer(new Pager2_ZoomInTransformer());
 
+        ViewPager2.OnPageChangeCallback onPageChangeCallback = new ViewPager2.OnPageChangeCallback() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                pagerCountText.setText(position+1 + "/" + goalTotalAdapter.getItemCount());
+            }
+        };
+
+        totalGoalList.registerOnPageChangeCallback(onPageChangeCallback);
         return root;
     }
 
