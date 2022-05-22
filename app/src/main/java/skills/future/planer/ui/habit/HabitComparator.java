1cc4f2dfb00c1849ab992d9125db1a1c7bb1bd79
Jarek <jarekopaczewski@protonmail.com>
2022-05-22 11:10:55 +0200

    - add sort to habit list

--- app/src/main/java/skills/future/planer/ui/habit/HabitComparator.java
+++ app/src/main/java/skills/future/planer/ui/habit/HabitComparator.java
@@ -1,18 +1,24 @@
 package skills.future.planer.ui.habit;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoField;
+import java.util.Calendar;
 import java.util.Comparator;
 
 import skills.future.planer.db.habit.HabitData;
 
 public class HabitComparator implements Comparator<HabitData> {
+
+    private long millis;
+
+    public HabitComparator(){
+        Calendar calendar = Calendar.getInstance();
+        millis = calendar.getTimeInMillis();
+    }
     @Override
     public int compare(HabitData x, HabitData y) {
-        long result = x.getBeginDay() - y.getBeginDay();
-        if( result < 0 )
-            return 1;
-        else if( result > 0 )
-            return -1;
-        else
-            return 0;
+        if( x.getEndDay() < millis) return 1;
+        long result = x.getEndDay() - y.getEndDay();
+        return result < 0 ? 1 : result > 0 ? -1 : 0;
     }
 }
